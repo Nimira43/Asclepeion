140f8cf3582458831f218522a99161edc924ccab
Nimira43 <n.rabone72@aol.com>
2025-01-19 23:24:04 +0000

    Tailwind setup.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1 className='logo'>Asclepeion</h1>
-      <h2 className='heading'>Medical care services</h2>
-      <p>24 Hour Support</p>
+    <div className='bg-[#111] p-5'>
+      <h1 className='logo text-[#eee] text-4xl'>Asclepeion</h1>
+      <h2 className='heading text-[#2874c6] text-2xl'>Medical care services</h2>
+      <p className='text-[#eee]'>24 Hour Support</p>
     </div>
   )
 }
