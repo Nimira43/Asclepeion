c000ceb8da869095fabc4c8a79431ba4e2e53fd8
Nimira43 <n.rabone72@aol.com>
2025-01-20 06:12:10 +0000

    Created Appointment page and provided route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
 
 const App = () => {
   return (
@@ -19,7 +20,8 @@ const App = () => {
         <Route path='/contact' element={<Contact />} />
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/my-appointments' element={<MyAppointments />} />
-        
+        <Route path='appointment/:docId' element={<Appointment />} />
+
       </Routes>
     </div>
   )
