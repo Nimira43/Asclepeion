dbe3a85808d43cd0b2d8f41e2a2294a6df1211e8
Nimira43 <n.rabone72@aol.com>
2025-01-20 07:13:25 +0000

    More styling on the li tags

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,24 +2,24 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-secondary-lg'>  
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-secondary'>  
       <h1 className='logo cursor-pointer text-primary'>Asclepeion</h1>
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink>
-          <li className='py-1 uppercase font-medium'>Home</li>
-          <hr className='border-none outline-none h-[1px] bg-secondary-lg w-3/5 m-auto' />
+          <li className='py-1 uppercase font-semibold text-dark hover:text-primary-lg'>Home</li>
+          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto' />
         </NavLink>
         <NavLink>
-          <li className='py-1 uppercase font-medium'>Doctors</li>
-          <hr className='border-none outline-none h-[1px] bg-secondary-lg w-3/5 m-auto' />
+          <li className='py-1 uppercase font-semibold text-dark hover:text-primary-lg'>Doctors</li>
+          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto' />
         </NavLink>
         <NavLink>
-          <li className='py-1 uppercase font-medium'>Contact</li>
-          <hr className='border-none outline-none h-[1px] bg-secondary-lg w-3/5 m-auto' />
+          <li className='py-1 uppercase font-semibold text-dark hover:text-primary-lg'>Contact</li>
+          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto' />
         </NavLink>
         <NavLink>
-          <li className='py-1 uppercase font-medium'>About</li>
-          <hr className='border-none outline-none h-[1px] bg-secondary-lg w-3/5 m-auto' />
+          <li className='py-1 uppercase font-semibold text-dark hover:text-primary-lg'>About</li>
+          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto' />
         </NavLink>
       </ul>
       <div>
