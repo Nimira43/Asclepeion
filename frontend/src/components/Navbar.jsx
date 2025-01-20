80b53bdcf89c00eff1cd52859564f1ff4568efd0
Nimira43 <n.rabone72@aol.com>
2025-01-20 09:43:18 +0000

    Styled the user menu to appear on hover.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,9 +31,7 @@ const Navbar = () => {
       </ul>
       <div className='flex items-center gap-4'>
         { token 
-          ? <div
-              className='flex items-center gap-2 cursor-pointer group relative'
-            >
+          ? <div className='flex items-center gap-2 cursor-pointer group relative'>
               <img
                 className='w-8 rounded-full' 
                 src={assets.profile} 
@@ -44,8 +42,8 @@ const Navbar = () => {
                 src={assets.dropdown_icon} 
                 alt='dropdown icon' 
               />
-              <div>
-                <div>
+              <div className= 'absolute top-0 right-0 pt-14 text-base font-semibold text-dark z-20 hidden group-hover:block'>
+                <div className='min-w-48 bg-grey-xl rounded flex flex-col gap-4 p-4'>
                   <p>My Profile</p>
                   <p>My Appointments</p>
                   <p>Logout</p>
