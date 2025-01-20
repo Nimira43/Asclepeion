f8c76fdd6e913aa6aa3b87752f2c95ac9a0a2305
Nimira43 <n.rabone72@aol.com>
2025-01-20 06:37:32 +0000

    Adding styling to the li and hr tags

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,24 +2,25 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#1880f0]'>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#1880f0]'>  
+      {/* Primary colour #0d6a0d Secondary colour #1880f0 */}
       <h1 className='logo cursor-pointer'>Asclepeion</h1>
-      <ul>
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink>
-          <li>Home</li>
-          <hr />
+          <li className='py-1'>Home</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
         </NavLink>
         <NavLink>
-          <li>Doctors</li>
-          <hr />
+          <li className='py-1'>Doctors</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
         </NavLink>
         <NavLink>
-          <li>Contact</li>
-          <hr />
+          <li className='py-1'>Contact</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
         </NavLink>
         <NavLink>
-          <li>About</li>
-          <hr />
+          <li className='py-1'>About</li>
+          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
         </NavLink>
       </ul>
       <div>
