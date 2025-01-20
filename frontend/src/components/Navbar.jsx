b886b0ab74c5018b11206d699117176ea63e9f48
Nimira43 <n.rabone72@aol.com>
2025-01-20 08:50:29 +0000

    Created two state variables  - one for show menu and the other for when user is logged in or not. Toekn will be true if logged in.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
   const navigate = useNavigate()
+  const [showMenu, setShowMenu] = useState(false)
+  const [token, setToken] = useState(true)
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-secondary'>  
