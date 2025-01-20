ed3d48536bff4ba5a242bb79ebc66022df793ad8
Nimira43 <n.rabone72@aol.com>
2025-01-20 07:38:26 +0000

    Added routes to NavLinks

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,21 +5,21 @@ const Navbar = () => {
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-secondary'>  
       <h1 className='logo cursor-pointer text-primary'>Asclepeion</h1>
       <ul className='hidden md:flex items-start gap-5 font-medium'>
-        <NavLink>
+        <NavLink to='/'>
           <li className='py-1 uppercase font-semibold text-dark hover:text-primary-lg'>Home</li>
-          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto' />
+          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto hidden' />
         </NavLink>
-        <NavLink>
+        <NavLink to='/doctors'>
           <li className='py-1 uppercase font-semibold text-dark hover:text-primary-lg'>Doctors</li>
-          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto' />
+          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto hidden' />
         </NavLink>
-        <NavLink>
+        <NavLink to='/contact'>
           <li className='py-1 uppercase font-semibold text-dark hover:text-primary-lg'>Contact</li>
-          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto' />
+          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto hidden' />
         </NavLink>
-        <NavLink>
+        <NavLink to='/about'>
           <li className='py-1 uppercase font-semibold text-dark hover:text-primary-lg'>About</li>
-          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto' />
+          <hr className='border-none outline-none h-[1px] bg-primary w-3/5 m-auto hidden' />
         </NavLink>
       </ul>
       <div>
