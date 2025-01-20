d547a23e8d338e456ada4d83d7931147518e1333
Nimira43 <n.rabone72@aol.com>
2025-01-20 04:14:02 +0000

    Added routes for Doctors.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Doctors from './pages/Doctors'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
       </Routes>
     </div>
   )
