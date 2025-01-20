91695b6585605f60f658477ef31c9f3306c2555c
Nimira43 <n.rabone72@aol.com>
2025-01-20 07:08:17 +0000

    Styled Navlinks and Register button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,29 +2,28 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-[#1880f0]'>  
-      {/* Primary colour #0d6a0d Secondary colour #1880f0 */}
-      <h1 className='logo cursor-pointer'>Asclepeion</h1>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-secondary-lg'>  
+      <h1 className='logo cursor-pointer text-primary'>Asclepeion</h1>
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink>
-          <li className='py-1'>Home</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+          <li className='py-1 uppercase font-medium'>Home</li>
+          <hr className='border-none outline-none h-[1px] bg-secondary-lg w-3/5 m-auto' />
         </NavLink>
         <NavLink>
-          <li className='py-1'>Doctors</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+          <li className='py-1 uppercase font-medium'>Doctors</li>
+          <hr className='border-none outline-none h-[1px] bg-secondary-lg w-3/5 m-auto' />
         </NavLink>
         <NavLink>
-          <li className='py-1'>Contact</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+          <li className='py-1 uppercase font-medium'>Contact</li>
+          <hr className='border-none outline-none h-[1px] bg-secondary-lg w-3/5 m-auto' />
         </NavLink>
         <NavLink>
-          <li className='py-1'>About</li>
-          <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto' />
+          <li className='py-1 uppercase font-medium'>About</li>
+          <hr className='border-none outline-none h-[1px] bg-secondary-lg w-3/5 m-auto' />
         </NavLink>
       </ul>
       <div>
-        <button>Register Today</button>
+        <button className='uppercase font-semibold bg-primary text-light px-3 py-2 rounded-md hover:bg-primary-lg'>Register Today</button>
       </div>
     </div>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#0d6a0d',
+        'primary-lg': '#17ac17',
+        'secondary': '#1880f0',
+        'secondary-lg': '#4395ed',
+        'dark': '#111',
+        'grey-dk': '#333',
+        'grey-md': '#888',
+        'grey-lg': '#ccc',
+        'grey-xl': '#eee',
+        'light': '#fff5f5'
+      }
+    },
   },
   plugins: [],
 }
